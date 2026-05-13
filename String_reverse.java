1858ea736185102520f0ba4374aee5f0e42cbcd7
Samiksha-1207 <samikshamalik1610@gmail.com>
2026-05-13 10:18:17 +0530

    Basic programs

--- String_reverse.java
+++ String_reverse.java
@@ -1,6 +1,8 @@
 public class String_reverse {
     public static void main(String[] args) {
         String sr="Samiksha";
-        
+        String reversed =new StringBuilder(sr).reverse().toString();
+        System.out.println("The string was: " + sr);
+        System.out.println("The reversed string is: " + reversed);
     }
 }
